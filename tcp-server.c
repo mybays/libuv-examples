381379b179c689d50df747d4e85a3e67a8011c07
mybays <mybays@qq.com>
2017-04-07 00:30:16 +0800

    tcp-server

--- tcp-server.c
+++ tcp-server.c
@@ -4,70 +4,101 @@
 
 uv_loop_t *loop;
 
+/* uv_alloc_cb示例
+* void (*uv_alloc_cb)(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
+*/
 void alloc_buffer(uv_handle_t *handle, size_t suggested_size,uv_buf_t* buf)
 {
-  //*buf=uv_buf_init((char*) malloc(suggested_size), suggested_size);
-  static char base[1024];
-  buf->base = base;
-  buf->len = sizeof(base);
+  printf("debug:buf0:%p\r\n",buf);
+  *buf=uv_buf_init((char*) malloc(suggested_size), suggested_size);
+  printf("debug:buf1:%p\r\n",buf);
+  //uv_buf_init前后的buf指针是一样的,为什么?
 }
 
+/* uv_write_cb示例
+* void (*uv_write_cb)(uv_write_t* req, int status)
+* 根据uv_write_t做收尾工作
+*/
 void echo_write(uv_write_t *req, int status)
 {
-  if (status == -1)
+  if (status < 0)
   {
-    fprintf(stderr, "Write error!\n");
+    printf("error:%s:%s:%s\r\n",__FUNCTION__,uv_strerror(status),uv_err_name(status));
   }
-  char *base = (char*) req->data;
-  //free(base);
+  //此req和uv_write的req是同一个
+
+
+  uv_buf_t* temp=(uv_buf_t*)req->data;
+
+  char *base = (char*) temp->base;
+  free(base);
   free(req);
 }
 
+/* uv_read_cb示例
+* void (*uv_read_cb)(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
+* 
+*/
 void echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t* buf)
 {
-  if (nread == -1)
+  if (nread < 0)
   {
+    if(nread == UV__EOF )
+    {
+      printf("remote closed.\r\n");
+    }
+
     fprintf(stderr, "Read error!\n");
     uv_close((uv_handle_t*)client, NULL);
     return;
   }
-  if(nread == UV__EOF )
+
+  if (nread == 0)
   {
-    printf("remote closed.\r\n");
+    printf("nread == 0\r\n");
     return;
   }
-  uv_buf_t sndbuf;
+
   printf("nread:%ld\r\n",nread);
+
+
+  
+  uv_buf_t sndbuf;
+
   uv_write_t *write_req = (uv_write_t*)malloc(sizeof(uv_write_t));
-  write_req->data = (void*)buf->base;
-  buf->len = nread;
-  sndbuf=*buf;
-  sndbuf.len=nread;
-  uv_write(write_req, client, &sndbuf, 1, echo_write);
+  write_req->data = (void*)buf;
+  
+  //uv_write的返回值是不是传给uv_write_cb函数了?
+  uv_write(write_req, client, write_req->data, 1, echo_write);
+
 }
 
+/* uv_connection_cb示例
+* void (*uv_connection_cb)(uv_stream_t* server, int status)
+* 有客户端连接时的操作
+*/
 void on_new_connection(uv_stream_t *server, int status)
 {
 
-  if(0 != status)
+  //status不为0则异常
+  if(0 > status)
   {
-    
-    return;
-  }
-  printf("status:%d\r\n",status);
-  if (status == -1)
-  {
+    printf("error:%s:%s\r\n",uv_strerror(status),uv_err_name(status));
     return;
   }
 
+  //uv_tcp_t里包含对端的各种信息,通过此信息来区分不同的client
   uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
   uv_tcp_init(server->loop, client);
-  if (uv_accept(server, (uv_stream_t*) client) == 0)
+  int r = uv_accept(server, (uv_stream_t*) client);
+
+  if (r == 0)
   {
     uv_read_start((uv_stream_t*) client, (uv_alloc_cb)alloc_buffer, (uv_read_cb)echo_read);
   }
   else
   {
+    printf("uv_accept error:%s:%s\r\n",uv_strerror(r),uv_err_name(r));
     uv_close((uv_handle_t*) client, NULL);
   }
 }
